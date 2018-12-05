343c649a2019c32e7f23d80a58c594f340d464f4
Fizzleboxer <ussoldier0286@gmail.com>
2018-12-04 22:01:11 -0600

    test

---
+++ CoderByte_FirstReverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderByte_FirstReverse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+			//For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+		}
+	}
+}
