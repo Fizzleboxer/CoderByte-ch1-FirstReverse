06c2fda292b664869f549c2525f0de36fa6a0c17
Fizzleboxer <ussoldier0286@gmail.com>
2018-12-04 22:02:03 -0600

    I added some strings.

--- CoderByte_FirstReverse/Program.cs
+++ CoderByte_FirstReverse/Program.cs
@@ -13,6 +13,8 @@ namespace CoderByte_FirstReverse
 			//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
 			//For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
 
+			//writing some code fuck yeah
+
 		}
 	}
 }
